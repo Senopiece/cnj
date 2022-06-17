a9eb8f15209a41744ebb167902c7010f66be9d17
Senopiece <shichiromail@gmail.com>
2022-06-17 23:58:14 +0300

    add crashlitics

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'dart:async';
+
 import 'package:cnj/pages/jokes.dart';
 import 'package:cnj/themes/main.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as flutter_services;
 import 'package:get/get.dart';
 
+import 'firebase_options.dart';
 import 'services/binding.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  flutter_services.SystemChrome.setPreferredOrientations([
-    flutter_services.DeviceOrientation.portraitUp,
-  ]);
-  runApp(const ChuckNorrisJokeApp());
+Future<void> main() async {
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    flutter_services.SystemChrome.setPreferredOrientations([
+      flutter_services.DeviceOrientation.portraitUp,
+    ]);
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    runApp(const ChuckNorrisJokeApp());
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
+  });
 }
 
 class ChuckNorrisJokeApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus_macos
+import firebase_core
+import firebase_crashlytics
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
